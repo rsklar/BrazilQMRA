a68f39077efed25ae5b0cf7acf2d2ce269ba6bb1
Rachel Sklar <rsklar@berkeley.edu>
2015-12-15 16:01:00 -0500

    added distribution for ratio

---
+++ swim4_a.1.R
@@ -0,0 +1,86 @@
+library(fitdistrplus)
+library(mc2d)
+concdatfresh<-read.csv("./concdatfresh.csv",header=TRUE,sep=",")
+
+ndvar(10001)
+
+#swim 4a uses the a neha suggests in her dose response for ecoli. 
+
+#Scenario: Swimming in freshwater:
+expon<-function(a,k,d){
+  return(a+(1-a)*(1-(exp(1)^(-k*d))))
+}
+  
+bpois<-function(n50,alpha,d){
+  return(1-(1+d*(2^(1/alpha)-1)/n50)^-alpha)
+}
+
+pathogen<-data.frame(
+  org     =c("ecoli",   "campylobacter", "salmonella", "rotavirus", "cryptosporidium", "ascaris"),
+  alpha   =c(NA,        0.145,           0.3126,       0.2531,      NA,                NA),
+  a       =c(0.000105,  1,               1,            1,           1,                 1),
+  n50     =c(NA,        896,             23600,        6.17,        NA,                NA),
+  pdi     =c(1,        0.3,             0.3,          0.5,         0.7,               0.39),
+  k       =c(0.0000511, NA,              NA,           NA,          0.00419,           0.0199),
+  ratio   =c(1,         10^5,            10^5,         10^5,        10^6,              10^6),
+  equation=c("expon",   "bpois",         "bpois",      "bpois",     "expon",           "expon")
+)
+
+#Convert FC to pathogen concentration
+convertpath_fresh<-function(ratio){
+  (concdatfresh$mpn.ml*0.843)/ratio
+}
+
+#For each pathogen go through the Monte Carlo Simulations
+
+simulator<-function(rowpath){
+  ratio<-as.numeric(rowpath["ratio"])
+  
+  #We decided concentration will be lognormally distributed
+  #Schets 2010 reports gamma distribution of ingestion and lorm distribution of duration(time)
+  
+  #r=ratio,dd=distribution environmental data,c=concentration
+  #ask patrick if i should run this as an mc stoch or not. 
+  r<-mcstoc(func=rlnorm,type="V",meanlog=ratio)
+  dd<-mcstoc(func=rlnorm,type="V",meanlog=mean(log(concdatfresh$mpn.ml)),sdlog=sd(log(concdatfresh$mpn.ml)))
+ #check to see if you should divide or multiply by the ratio. 
+  c<-dd*0.843/r
+  i<-mcstoc(func=rgamma,type="V",rate=0.45,shape=60)
+  t<-mcstoc(func=rlnorm,type="V",meanlog=3.6,sdlog=0.85)
+  d<-c*i*t
+  
+  riski<<-if(rowpath["equation"]=="expon"){
+    a<-as.numeric(rowpath["a"])
+    k<-as.numeric(rowpath["k"])
+    expon(a,k,d)
+  } else if(rowpath["equation"]=="bpois"){
+    n50<-as.numeric(rowpath["n50"])
+    alpha<-as.numeric(rowpath["alpha"])
+    bpois(n50,alpha,d)
+  }
+  
+  riskd <- riski *as.numeric(rowpath["pdi"])
+
+  #risk infection per event
+  if(rowpath["org"] != "ecoli"){
+    print(rowpath["org"])
+    swiminf<<-mc(c,i,t,d,riski)
+    print(swiminf) 
+    #print(summary(swiminf))
+  }
+  
+  #risk disease per event 
+  print(rowpath["org"])
+  swimdis<-mc(c,i,d,riskd)
+  print(swimdis)
+  #print(summary(swimdis))
+  #plot(swimdis, na.rm=TRUE)
+  
+  
+}
+
+(apply(pathogen,1,simulator))
+
+
+
+
